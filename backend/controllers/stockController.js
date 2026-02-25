a3fd8f7c2204edbb20dbab9cd687297df7691b5c
Dhrub Kumar Garg <dhrub.kumargarg2024@vitstudent.ac.in>
2026-02-25 11:27:54 +0530

    Fixing

--- backend/controllers/stockController.js
+++ backend/controllers/stockController.js
@@ -1,5 +1,6 @@
 const Stock = require("../models/Stock");
-const yahooFinance = require("yahoo-finance2").default;
+const YahooFinance = require("yahoo-finance2").default;
+
 const yahooFinance = new YahooFinance();
 
 /* ===========================
